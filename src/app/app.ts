aff1dee6bb30892d10d34e4b6ebd4320d20c5dd9
Gabriela Justino <gabrielajustinodesouza@gmail.com>
2025-07-18 21:30:26 -0300

    criando metodos para calcular

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,61 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'calculadora';
+
+  resultadoDisplay: string = '';
+  numAnterior: string = '';
+  numProximo: string = '';
+  operacao: string = '';
+
+  apresentarNum(num: String){
+    this.numAnterior += num;
+    this.resultadoDisplay = this.numAnterior;
+  }
+
+  setOperacao(op: string) {
+    if (this.numAnterior === '') return;
+
+    this.numProximo = this.numAnterior;
+    this.numAnterior = '';
+    this.operacao = op;
+    this.resultadoDisplay = `${this.numProximo} ${op}`;
+  }
+
+  calcular(){
+    let resultado: number;
+
+    const anter = parseFloat(this.numAnterior);
+    const prox = parseFloat(this.numProximo);
+
+    if (isNaN(anter) || isNaN(prox)) return;
+
+    switch (this.operacao) {
+      case '+':
+        resultado = anter + prox;
+        break;
+      case '-':
+        resultado = anter - prox;
+        break;
+      case '*':
+        resultado = anter * prox;
+        break;
+      case '/':
+        resultado = anter / prox;
+        break;
+      default:
+        return;
+    }
+      this.resultadoDisplay = resultado.toString();
+      this.numAnterior = resultado.toString();
+      this.numProximo = '';
+      this.operacao = '';
+  }
+
+  //limpar o display
+  limpar(){
+    this.resultadoDisplay = '';
+    this.numAnterior = '';
+    this.numProximo = '';
+    this.operacao = '';
+  }
 }
